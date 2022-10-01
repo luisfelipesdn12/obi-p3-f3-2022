7dce08e4e76c107bea73acf89630e90bc8d0baa1
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2022-10-01 14:54:55 -0300

    pilhas: solucao

--- pilhas/pilhas.py
+++ pilhas/pilhas.py
@@ -2,10 +2,14 @@ n, k = map(int, input().split())
 alturas = list(map(int, input().split()))
 moedas = 0
 
-# 13 pontos com k = 1
-alt_max = max(alturas)
-for i in range(len(alturas)):
-    # print(i)
-    moedas += alt_max - alturas[i]
+# os K maiores valores
+k_max = sorted(alturas, reverse=True)[0:k]
+
+# o menos maior valor
+min_max = min(k_max)
+
+for a in alturas:
+    if a < min_max:
+        moedas = min_max - a
 
 print(moedas)

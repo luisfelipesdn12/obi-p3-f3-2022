8d58233e12c701f14c1a0664a339eaf0feb17ef2
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2022-10-01 14:05:14 -0300

    digitos first try w comments

---
+++ digitos/digitos.py
@@ -0,0 +1,38 @@
+n = int(input())
+digitos = input().split()
+
+# tamanho do teste
+t = 1
+
+while t <= n:
+    # digito anterior
+    da = None
+    
+    # posição inicial do teste
+    p = 0
+    
+    # tamanho está errado
+    wrong = False
+
+    while p+t <= n:
+        # dígito obtido
+        d = int("".join(digitos[p:p+t]))
+        print(d)
+
+        if da == None: pass
+        elif da + 1 == d: pass
+        else:
+            wrong = True
+            break
+
+        da = d
+        p += t
+
+    print(wrong)
+    
+    if wrong == False: break
+
+    t += 1
+
+a = int("".join(digitos[0:t]))
+print(a)

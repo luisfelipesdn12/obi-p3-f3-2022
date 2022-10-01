615f4f9dea0033a0ae31448eccbb31e7bd0a108f
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2022-10-01 15:08:10 -0300

    pilhas: fix

--- pilhas/pilhas.py
+++ pilhas/pilhas.py
@@ -3,13 +3,16 @@ alturas = list(map(int, input().split()))
 moedas = 0
 
 # os K maiores valores
-k_max = sorted(alturas, reverse=True)[0:k]
+k_max = sorted(set(alturas), reverse=True)[0:k]
+# print(f"k_max: {k_max}")
 
 # o menos maior valor
 min_max = min(k_max)
+# print(f"min_max: {min_max}")
 
 for a in alturas:
+    # print(f"a: {a} ({min_max - a})")
     if a < min_max:
-        moedas = min_max - a
+        moedas += min_max - a
 
 print(moedas)
